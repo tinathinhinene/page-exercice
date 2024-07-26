6916ca37329ded3e1c74cacfe29f2ccb4df613aa
tinathinhinene <tina.goblet@gmail.com>
2024-07-26 12:17:03 +0200

    t

--- sitexo.js
+++ sitexo.js
@@ -1,9 +1,25 @@
-//***  ceci est une barre de nav */
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger= document.querySelector(".hamburger");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
 
-function toggleMenu(){
-
-const menu= document.querySelector('.menu');
-menu.style.display=(menu.style.display==='block')?'none' : 'block';
-
-
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
 }
+
+hamburger.addEventListener("click", toggleMenu);
+
+menuItems.forEach( 
+  function(menuItem) { 
+    menuItem.addEventListener("click", toggleMenu);
+  }
+)
