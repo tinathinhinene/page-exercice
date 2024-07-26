5983d7179c789418c614e5d3b80f4f5669c432f5
tinathinhinene <tina.goblet@gmail.com>
2024-07-26 08:38:40 +0200

    t

---
+++ sitexo.js
@@ -0,0 +1,9 @@
+//***  ceci est une barre de nav */
+
+function toggleMenu(){
+
+const menu= document.querySelector('.menu');
+menu.style.display=(menu.style.display==='block')?'none' : 'block';
+
+
+}
